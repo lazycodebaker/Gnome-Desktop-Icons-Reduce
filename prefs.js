da58391ff31ea415235f6355e8f6e0905bf2adf4
Anshuman Tiwari <87172624+lazycodebaker@users.noreply.github.com>
2021-08-15 00:50:23 -0400

    Create prefs.js

---
+++ prefs.js
@@ -0,0 +1,5 @@
+
+const ICON_SIZE = { 'small': 40, 'standard': 48, 'large': 64 };
+const ICON_WIDTH = { 'small': 85, 'standard': 95, 'large': 95 };
+const ICON_HEIGHT = { 'small': 75, 'standard': 85, 'large': 90 };
+
